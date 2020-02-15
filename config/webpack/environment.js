a83240f137de14e57db6d083e887999e03759614
Coralie Delpha <coralie.delpha@gmail.com>
2020-02-16 00:52:38 +0100

    solved map display pb - Prevent Babel from transpiling node_modules packages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+
+module.exports = environment;
+
